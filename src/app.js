60def02890f69c23b632d4ed3a12d0b47a081ffc
unknown <nirupama1210@gmail.com>
2021-09-27 17:50:10 +0530

    PROGRAD 5622

--- src/app.js
+++ src/app.js
@@ -24,6 +24,27 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
+    for(let i=0;i<players.length;i++)
+    {
+        if(i%2==0)
+        {
+        detailedPlayers[i]={
+            name:players[i],
+            strength: getRandomStrength(),
+            image: "images/super-"+(i+1)+".png",
+            type: "hero"
+        };
+    }
+        else{
+            detailedPlayers[i]={
+                name:players[i],
+                strength: getRandomStrength(),
+                image: "images/super-"+(i+1)+".png",
+                type: "villain"
+            };
+        }
+    }
+    
     // Create players using for loop
     // Type your code here
 
@@ -34,6 +55,8 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    let val=Math.ceil(Math.random()*100+1);
+    return val;
 }
 
 const buildPlayers = (players, type) => {
@@ -42,8 +65,21 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    if(type=='hero')
+    {
+    for(let i=0;i<players.length;i+=2)
+    {
+        fragment=fragment+` <div class="player"><img src="${players[i].image}" alt=""><div class="name">${players[i].name}</div><div class="strength">${players[i].strength}</div></div>`;
+    }
     return fragment;
+    }
+    else{
+        for(let i=1;i<players.length;i+=2)
+        {
+            fragment=fragment+` <div class="player"><img src="${players[i].image}" alt=""><div class="name">${players[i].name}</div><div class="strength">${players[i].strength}</div></div>`;
+        }
+        return fragment; 
+    }
 }
 // Display players in HTML
 const viewPlayers = (players) => {
